29ea4f484400f10794250ab7a5ffc6d0cc9563b3
HiteshAttri <hiteshattri71@gmail.com>
2022-10-29 20:00:28 +0530

    test program_ commit 2

---
+++ TestPackage/Practical_22Test.java
@@ -0,0 +1,7 @@
+package TestPackage;
+
+public class Practical_22Test {
+    public static void main(String[] args) {
+        System.out.println("this .java file is created in IntellIJ , testing github file version control");
+    }
+}
